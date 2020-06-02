81332818b37d2dcbc51e181de1f0b7ab56a598b9
Inna Romankova <romankova03071978@gmail.com>
2020-06-02 16:41:43 +0300

    Existing selenium tests refactoring according new project structure. (Booking)

---
+++ src/test/java/runners/BookingRunner.java
@@ -0,0 +1,13 @@
+package runners;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.booking.BookingParisHouseTest;
+import tests.booking.BookingMoskowHouseTest;
+import tests.booking.BookingOsloHouseTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({BookingParisHouseTest.class, BookingMoskowHouseTest.class, BookingOsloHouseTest.class})
+
+public class BookingRunner {
+}
---
+++ src/test/java/tests/booking/BookingMoskowHouseTest.java
@@ -0,0 +1,70 @@
+package tests.booking;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import steps.BaseSteps;
+import steps.booking.SpecialStepss;
+import webDrivers.Config;
+import webDrivers.DriverManager;
+
+import java.util.concurrent.TimeUnit;
+
+public class BookingMoskowHouseTest {
+    String date = null;
+    int daysAmount = 5;
+    int daysShift = 10;
+
+    WebElement element;
+    WebDriver driver;
+
+    @Before
+    public void preCondition() {
+        driver = DriverManager.getDriver(Config.CHROME);
+        driver.get("https://www.booking.com/");
+    }
+
+    @Test
+    public void booking2Test() throws InterruptedException {
+        BaseSteps.findElementSendKeys(driver, "//*[@id=\"ss\"]", "Moscow");  //set City: Moscow
+        BaseSteps.findElementClick(driver, "//*[contains(@class, \"xp__input-group xp__date-time\")]");
+        BaseSteps.findElementClick(driver, String.format("//*[contains(@data-date, \"%s\")]", SpecialStepss.setDays
+                (daysShift)));
+        BaseSteps.findElementClick(driver, String.format("//*[contains(@data-date, \"%s\")]", SpecialStepss.setDays
+                (daysAmount + daysShift)));  //set days
+        BaseSteps.findElementClick(driver, "//*[contains(@type, \"submit\")]");
+        TimeUnit.SECONDS.sleep(3);
+
+        Actions actions = new Actions (driver);
+        element = driver.findElement(By.xpath("//*[@id=\"group_adults\"]"));
+        actions.moveToElement(element).click().sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).click().perform();
+
+        element = driver.findElement(By.xpath("//*[@id=\"no_rooms\"]"));
+        actions.moveToElement(element).click().sendKeys(Keys.ARROW_DOWN).click().perform();
+
+        BaseSteps.findElementClick(driver, "//*[contains(@class, \"sort_price\")]/a");
+        element = BaseSteps.findElementClickReturn(driver, "//*[@id=\"filter_price\"]//a[1]");
+        String maxPrice = element.getText();
+        maxPrice = maxPrice.replaceAll("([^1-9][^0-9]+)", "");
+        TimeUnit.SECONDS.sleep(2);
+
+        String firstPrice = BaseSteps.findElementGetText(driver, "//*[contains(@class, \"bui-price-display\")]" +
+                "/div[2]/div");
+
+        firstPrice = firstPrice.replaceAll("\\D+", "");
+        int firstOneDayPrice = Integer.parseInt(firstPrice) / (daysAmount - daysShift);
+        System.out.println("Price: up to " + maxPrice + "; Min one Night Price: " + firstOneDayPrice);
+        Assert.assertTrue(firstOneDayPrice <= Integer.parseInt(maxPrice));
+    }
+
+    @After
+    public void postCondition() {
+        BaseSteps.destroyDriver(driver);
+    }
+}
